a11bc1ffe0528fedccf4c5280e60770ea20fa734
nando <muhammad.rezki.ananda@gmail.com>
2021-09-21 08:34:19 +0700

    INITIAL COMMIT

---
+++ bin/smart_calculator.dart
@@ -0,0 +1,5 @@
+import 'package:smart_calculator/smart_calculator.dart' as smart_calculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${smart_calculator.calculate()}!');
+}
---
+++ test/smart_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:smart_calculator/smart_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
