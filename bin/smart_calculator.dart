332966cb77e748a595d33fc8da293cd71ab5950f
nando <muhammad.rezki.ananda@gmail.com>
2021-09-21 11:07:05 +0700

    Add history

--- bin/smart_calculator.dart
+++ bin/smart_calculator.dart
@@ -1,13 +1,97 @@
-import '../lib/do_operation.dart';
-import '../lib/data_number.dart';
+import 'dart:io';
+
+import 'package:smart_calculator/data_number.dart';
+import 'package:smart_calculator/do_operation.dart';
+import 'package:smart_calculator/history.dart';
 
 void main(List<String> arguments) {
-  var operation = DoOperation(DataNumber(15, 25));
+  var history = <History>[];
+  print('''
+  SELAMAT DATANG DI KALKULATOR PALING PINTAR
+  ******************************************
+  ''');
 
-  print(operation.addition());
-  print(operation.subtraction());
-  print(operation.multiplied());
-  print(operation.division());
-  print(operation.heighestCommonFactor());
-  print(operation.lowestCommonMultiple());
+  do {
+    print('''
+  \n
+  Masukkan menu : 
+  1. Penjumlahan     4. Pembagian
+  2. Pengurangan     5. FPB
+  3. Perkalian       6. KPK
+          0. History
+  ''');
+
+    print('Masukkan pilihan Anda  :');
+    var option = int.tryParse(stdin.readLineSync() ?? '0');
+    switch (option) {
+      case 1:
+        var number = inputNumber();
+        var result = doOperation(number);
+        print('Hasil dari penjumlahan bilangan ${number[0]} dan ${number[1]}');
+        print('-->>> ${result.addition()}');
+        history.add(History(number, '+', result.addition()));
+        break;
+      case 2:
+        var number = inputNumber();
+        var result = doOperation(number);
+        print('Hasil dari pengurangan bilangan ${number[0]} dan ${number[1]}');
+        print('-->>> ${result.subtraction()}');
+        history.add(History(number, '-', result.subtraction()));
+        break;
+      case 3:
+        var number = inputNumber();
+        var result = doOperation(number);
+        print('Hasil dari perkalian bilangan ${number[0]} dan ${number[1]}');
+        print('-->>> ${result.multiplied()}');
+        history.add(History(number, '*', result.multiplied()));
+        break;
+      case 4:
+        var number = inputNumber();
+        var result = doOperation(number);
+        print('Hasil dari pembagian bilangan ${number[0]} dan ${number[1]}');
+        print('-->>> ${result.division()}');
+        history.add(History(number, '/', result.division()));
+        break;
+      case 5:
+        var number = inputNumber();
+        var result = doOperation(number);
+        print('Hasil dari FPB bilangan ${number[0]} dan ${number[1]}');
+        print('-->>> ${result.highestCommonFactor()}');
+        history.add(History(number, 'FPB', result.highestCommonFactor()));
+        break;
+      case 6:
+        var number = inputNumber();
+        var result = doOperation(number);
+        print('Hasil dari KPK bilangan ${number[0]} dan ${number[1]}');
+        print('-->>> ${result.lowestCommonMultiple()}');
+        history.add(History(number, 'KPK', result.lowestCommonMultiple()));
+        break;
+      case 0:
+        var listHistory = history
+            .map((e) => {
+                  'Hasil dari ${e.number[0]} ${e.operator} ${e.number[1]} = ${e.result}'
+                })
+            .toList();
+        print('Daftar Riwayat');
+        for (var i = 0; i < history.length; i++) {
+          print(listHistory[i].toString());
+        }
+        break;
+      default:
+        print('Anda memasukkan pilihan yang salah');
+    }
+  } while (true);
+}
+
+List inputNumber() {
+  print('Masukkan angka pertama :');
+  var numberA = double.parse(stdin.readLineSync() ?? '0');
+  print('Masukkan angka kedua :');
+  var numberB = double.parse(stdin.readLineSync() ?? '0');
+
+  return [numberA, numberB];
+}
+
+DoOperation doOperation(List number) {
+  return DoOperation(DataNumber(number[0], number[1]));
 }
---
+++ lib/history.dart
@@ -0,0 +1,9 @@
+import 'package:smart_calculator/do_operation.dart';
+
+class History {
+  final List number;
+  final String operator;
+  final double result;
+
+  History(this.number, this.operator, this.result);
+}
